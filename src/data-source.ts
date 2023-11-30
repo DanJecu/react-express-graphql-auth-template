5c3f38b03b76e9e6cd6d2a1ed9f48b7b0944e945
danJecu <jdanalexandru@gmail.com>
2023-11-30 08:41:54 +0100

    docker config

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,11 +4,11 @@ import { User } from "./entity/User";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 2022,
-  username: "postgres",
-  password: "postgres",
-  database: "postgres",
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT!,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: true,
   logging: false,
   entities: [User],
